d5a79b93f3431d64f7d8034f4c0c4d2150d62543
tony2020edx <64748043+tony2020edx@users.noreply.github.com>
2021-06-02 23:13:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+import pymysql
+
+import time
+
+start = time.time()
+
+
+
+
+data = pd.read_csv("headphones-master_data.csv")
+
+link_list = data['Product_url'].tolist()
+
+price_list = data['Sale_price'].tolist()
+
+crawled_date = time.strftime('%Y-%m-%d')
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='passme123@#$',
+                             db='hpsize')
+
+
+my_cursor = connection.cursor()
+
+
+for i in range(len(link_list)):
+
+    link = link_list[i]
+
+    price = price_list[i]
+
+
+
+    sql = "INSERT INTO comparison (link, price, crawled_date) VALUES (%s, %s, %s)"
+
+    val = link , price , crawled_date
+
+
+    my_cursor.execute(sql, val)
+
+    connection.commit()
+
+
+my_cursor.execute("SELECT * from comparison")
+
+result = my_cursor.fetchall()
+
+for i in result:
+    print(i)
+
+connection.close()
+
+end = time.time()
+
+print(end - start)
