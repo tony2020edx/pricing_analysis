d3d8d9b6d7ac1321b6a1f9702a9d501542d62038
tony2020edx <64748043+tony2020edx@users.noreply.github.com>
2021-06-10 16:26:52 +0530

    Minor improvements

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ sql1 = "SELECT price FROM dummy WHERE crawled_date BETWEEN '2021-06-13' AND '202
 
 sql2 = "SELECT price FROM dummy WHERE crawled_date BETWEEN '2021-06-13' AND '2021-06-15' AND link ='https://www.flipkart.com/bose-noise-cancelling-700-anc-enabled-bluetooth-headset/p/itma57a01d3bd591?pid=ACCFGYZEVVGYM8FP'"
 
-sql =  "SELECT price FROM dummy WHERE link ='https://www.flipkart.com/bose-noise-cancelling-700-anc-enabled-bluetooth-headset/p/itma57a01d3bd591?pid=ACCFGYZEVVGYM8FP'"
+sql = "SELECT price FROM dummy WHERE link ='https://www.flipkart.com/bose-noise-cancelling-700-anc-enabled-bluetooth-headset/p/itma57a01d3bd591?pid=ACCFGYZEVVGYM8FP'"
 
 # the variables to be addded to the SQL query
 
@@ -21,6 +21,8 @@ result = list(my_cursor.fetchall())
 
 print(result)
 
+new_list = [i[0] for i in result]
 
+print(new_list)
 
 connection.close()
