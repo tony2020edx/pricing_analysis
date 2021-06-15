eeb0bd4d9805488c42d96c02b0b96e28841de65d
tony2020edx <64748043+tony2020edx@users.noreply.github.com>
2021-06-15 20:08:13 +0530

    fixed issues

--- analysis.py
+++ analysis.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 df = pd.read_csv("kbk.csv")
 
 link_list = df['link'].tolist()
@@ -15,15 +14,14 @@ unique_dates = set(dates)
 
 change_list = []
 
-precentage_change_in_price = []
-
+precentage_change = []
 
 offset_value = len(unique_urls)
 
 limiting_value = len(link_list) - offset_value
 
 
-def percentage_change(old_price,new_price):
+def percentage_change(old_price, new_price):  # get the percentage change by accepting two inputes
 
     change_metric = new_price - old_price
 
@@ -32,12 +30,11 @@ def percentage_change(old_price,new_price):
         price_change_percentage = 0
     else:
 
-        price_change_percentage = (change_metric/old_price)*100
+        price_change_percentage = (change_metric / old_price) * 100
 
-        price_change_percentage = round(price_change_percentage,2)
+        price_change_percentage = round(price_change_percentage, 2)
 
     return price_change_percentage
-
 
 
 for count in range(len(link_list)):
@@ -47,7 +44,7 @@ for count in range(len(link_list)):
         change = 0
 
         change_list.append(change)
-        precentage_change_in_price.append(change)
+        precentage_change.append(change)
 
     else:
 
@@ -63,36 +60,15 @@ for count in range(len(link_list)):
 
             change_list.append(change)
 
-            get_change = percentage_change(price2,price1)
+            get_change = percentage_change(price2, price1)
 
-            precentage_change_in_price.append(get_change)
+            precentage_change.append(get_change)
 
         else:
 
             pass
 
-
-
-
-
 print(change_list)
-print(precentage_change_in_price)
+print(precentage_change)
 
 print(len(change_list))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
